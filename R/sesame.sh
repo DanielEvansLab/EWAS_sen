efbd98c244186bfe0c43b047e97b7de5b8b57760
Dan Evans <danev1@gmail.com>
2020-08-14 02:30:28 -0700

    QC and probe assoc

--- R/sesame.sh
+++ R/sesame.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=20G
-#SBATCH --time=8:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=out/sesame.out
 #SBATCH --job-name="sesame"
 #SBATCH --mail-user=danev1@gmail.com
