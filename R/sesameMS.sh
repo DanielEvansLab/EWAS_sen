fb5392aa9bfff814a903e3aa78526bbd64fa6051
Dan Evans <danev1@gmail.com>
2021-07-08 19:41:46 -0700

    PCA chip

--- R/sesameMS.sh
+++ R/sesameMS.sh
@@ -4,16 +4,21 @@
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=20G
 #SBATCH --time=5:00:00
-#SBATCH --output=out/sesame.out
+#SBATCH --output=out/sesameMS.out
 #SBATCH --job-name="sesame"
 #SBATCH --mail-user=danev1@gmail.com
 #SBATCH --mail-type=ALL
-#SBATCH -p batch
+#SBATCH -p evanslab
 
 
 cd $SLURM_SUBMIT_DIR
 
 hostname
+module unload R
+module load R/4.0.3_gcc-8.3.0
 
 Rscript -e "rmarkdown::render('sesameMS.Rmd')"
+
+module unload R/4.0.3_gcc-8.3.0
+module load R
 
