707be9faf3480abab70ce130d739f73ab2ada2e0
Dan Evans <danev1@gmail.com>
2021-08-13 09:32:59 -0700

    pca done

--- R/sesameMS.sh
+++ R/sesameMS.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=20G
-#SBATCH --time=5:00:00
+#SBATCH --time=10:00:00
 #SBATCH --output=out/sesameMS.out
 #SBATCH --job-name="sesame"
 #SBATCH --mail-user=danev1@gmail.com
@@ -15,10 +15,10 @@ cd $SLURM_SUBMIT_DIR
 
 hostname
 module unload R
-module load R/4.0.3_gcc-8.3.0
+module load R/4.1.0_gcc-8.3.0
 
 Rscript -e "rmarkdown::render('sesameMS.Rmd')"
 
-module unload R/4.0.3_gcc-8.3.0
+module unload R/4.1.0_gcc-8.3.0
 module load R
 
